578866ed63bc9e3e2780b63a14ad2b01228fe001
Jørgen Bordal Andersn <jorgen.bordal@outlook.com>
2018-02-16 19:00:09 +0100

    kommentert ut ting som jeg ikke vet hvordan virker.

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.use(express.static("./public"));
 
+/*app.post('/user'), function(req, res) {
+    res.send('post request to the homepage');
+} */
+
+// let dbString = "postgres://npqasiwaemucmm:e3957a2831921db77732a6677da46574476957600c649568b449a8442764f3e7@ec2-54-247-101-202.eu-west-1.compute.amazonaws.com:5432/d8m8khsgfiqpk9";
+
 app.listen(process.env.PORT || 8080, function () {
-  console.log('everything is good');
+    console.log('everything is good');
 });
