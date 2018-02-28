c01bbbd4f38893e57fea88eec991e1cb607b66dc
Jørgen Bordal Andersn <jorgen.bordal@outlook.com>
2018-02-28 19:55:31 +0100

    fortsatt ingen fremskritt

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 8080;
+const {Client} = require("pg");
 
 app.use(express.static("./public"));
 
 
-
 app.listen(process.env.PORT || 8080, function () {
-    console.log('everything is good');
+    console.log('Listening');
 });
