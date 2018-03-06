81e5c5d148f6eea1f57dab9c94df96695279f957
Jørgen Bordal Andersn <jorgen.bordal@outlook.com>
2018-03-07 00:56:13 +0100

    stuff

--- server.js
+++ server.js
@@ -19,49 +19,12 @@ app.listen(process.env.PORT || 8080, function () {
     console.log('Listening');
 });
 
-/*
+/* Something like this. 
 
-app.get("/que", function (req, res) {
-    res.json(que).end();
-});
+app.post('/', function(req, res) {
 
-app.delete("/que/:name/",auth, function (req, res) {
-    let name = req.params.name;
+    res.send('Username: ' + req.body.username);
 
-    console.log("Delete " + name);
+};
 
-    if (name) {
-        name = name.toLowerCase();
-        let index = que.findIndex(item => {
-            return item.name === name;
-        });
-
-        if (index >= 0) {
-            complete.push(name);
-            que.splice(index, 1);
-        }
-
-        res.json(que);
-    }
-
-});
-
-app.post('/que/',  function (req, res) {
-
-    let name = req.body.name;
-    let info = req.body.info;
-
-    if (name) {
-        name = name.toLowerCase();
-        let index = que.findIndex(item => {
-            return item.name === name;
-        })
-
-        if (index == -1) {
-            que.push({name:name, info:info});
-        }
-
-    }
-
-    res.json(que);
-}); */
+*/
