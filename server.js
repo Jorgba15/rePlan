2bb6519aae3f1328e8eade25e8ce008594e78cab
Jørgen Bordal Andersn <jorgen.bordal@outlook.com>
2018-03-12 14:23:08 +0100

    had to reinstall npm

--- server.js
+++ server.js
@@ -9,29 +9,19 @@ app.use(express.static("./public"));
 
 let db = require('./dbconnect.js');
 
-
+/* Don't use this
 app.get('/',auth, function (req, res) {
     let staticApp = readTextFile("public/lists.html");
     res.send(staticApp);
 });
 
+*/
+// ENDPOINTS
+
+
+
+
+// Listen to port --------------------------------
 app.listen(process.env.PORT || 8080, function () {
     console.log('Listening');
 });
-
-/* Database-ting --------------------------------------------------------------
-
-app.use(function(req, res, next) {
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    next();
-});
-
-
-app.post('/user', function(req, res) {
-
-    res.send('Username: ' + req.body.username);
-    res.send('password: ' + req.body.password);
-
-};
-*/
