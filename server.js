2d7fa1d2e2587066b465d39b6766a9ade5593e2a
Jørgen Bordal Andersn <jorgen.bordal@outlook.com>
2018-03-12 15:51:21 +0100

    stuff

--- server.js
+++ server.js
@@ -2,26 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const auth = require('./authentication/authorize.js')("admin",process.env.superUser || "password");
+const auth = require('./auth.js');
 
 app.use(bodyParser.json());
 app.use(express.static("./public"));
 
 let db = require('./dbconnect.js');
 
-/* Don't use this
+/* Don't use this right now
 app.get('/',auth, function (req, res) {
-    let staticApp = readTextFile("public/lists.html");
+    let staticApp = readTextFile(".public/lists.html");
     res.send(staticApp);
 });
 
 */
 // ENDPOINTS
 
+app.post("/user", function(req, res) {
+    // Post request from createUser.html and loginUser.html is handeled here.
+    console.log("hm.");
+
+})
 
 
-
-// Listen to port --------------------------------
+// Tell app to Listen to port --------------------------------
 app.listen(process.env.PORT || 8080, function () {
     console.log('Listening');
 });
