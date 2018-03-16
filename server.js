66fa2975aa14dcb5f3ab6fc67a2f76e074e95bf4
Jørgen Bordal Andersn <jorgen.bordal@outlook.com>
2018-03-16 18:04:32 +0100

    found the mistake

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 var path = require('path');
 const app = express();
-
+app.use(express.static('public'));
 app.use(bodyParser.json());
 
 let db = require('./dbconnect.js');
