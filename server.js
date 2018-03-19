7524ada0ba1b9b24d2673314f6acb009fe4ecbcb
Jorgba15 <jorgen.bordal@outlook.com>
2018-03-19 13:32:01 +0100

    added missing module require

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
+const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
 // const bcrypt = require('bcrypt');
 
